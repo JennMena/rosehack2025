34cea14221256cb8178bcd63d480052b93935d29
Nina S <n.m.sokolnikova06@gmail.com>
2025-01-25 19:00:50 -0800

    fix all dashboard images

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -8,9 +8,7 @@ const DashboardPage: React.FC = () => {
                 <div className="w-[1094px] h-[3146px] left-[63px] top-[83px] absolute bg-[#fefae0]" />
                 <div className="w-[1094px] h-[3146px] left-[63px] top-[83px] absolute bg-[#fefae0]" />
                 <div className="w-[1208.29px] h-[105.91px] left-[13px] top-[2940.68px] absolute">
-                    <img className="w-[450.01px] h-[84.32px] left-[2.91px] top-[5.50px] absolute origin-top-left rotate-[2.06deg]" src="https://via.placeholder.com/450x84" />
-                    <img className="w-[450.01px] h-[84.32px] left-[380.21px] top-[3.75px] absolute origin-top-left rotate-[2.06deg]" src="https://via.placeholder.com/450x84" />
-                    <img className="w-[450.01px] h-[84.32px] left-[758.57px] top-0 absolute origin-top-left rotate-[2.06deg]" src="https://via.placeholder.com/450x84" />
+                    <img className="w-[1423px] h-[70px] left-[0px] top-[0px]" src="Vine.png" />
                 </div>
                 <div className="h-[753px] pb-3.5 left-[152px] top-[2167px] absolute flex-col justify-start items-start inline-flex">
                     <div className="w-[897px] h-[218px] relative">
@@ -22,15 +20,13 @@ const DashboardPage: React.FC = () => {
                         <div className="w-[897px] h-[60px] left-0 top-[461px] absolute text-[#412207] text-[32px] font-light font-['Lexend']">hint: start with small talk to build an initial connection.<br />(hints from the ai?)</div>
                         <div className="w-[573px] h-[347px] left-[324px] top-0 absolute bg-[#606c38]/60" />
                         <div className="w-[573px] h-[63px] left-[324px] top-[366px] absolute bg-[#606c38]/20" />
-                        <img className="w-[300px] h-[430.71px] left-0 top-0 absolute" src="https://via.placeholder.com/300x431" />
+                        <img className="w-[300px] h-[430.71px] left-0 top-0 absolute" src="Annette.png" />
                         <div className="w-[424px] h-[61px] left-[335px] top-[261px] absolute text-[#402207] text-[28px] font-light font-['Lexend']">Hello, I am a recruiter from Company Name.</div>
                         <div className="w-[546px] h-[42px] left-[335px] top-[377px] absolute text-[#402207]/60 text-[28px] font-light font-['Lexend']">type here</div>
                     </div>
                 </div>
                 <div className="w-[1208.29px] h-[105.91px] left-[3px] top-[2041.10px] absolute">
-                    <img className="w-[450.01px] h-[84.32px] left-[2.91px] top-[5.50px] absolute origin-top-left rotate-[2.06deg]" src="https://via.placeholder.com/450x84" />
-                    <img className="w-[450.01px] h-[84.32px] left-[380.21px] top-[3.75px] absolute origin-top-left rotate-[2.06deg]" src="https://via.placeholder.com/450x84" />
-                    <img className="w-[450.01px] h-[84.32px] left-[758.57px] top-0 absolute origin-top-left rotate-[2.06deg]" src="https://via.placeholder.com/450x84" />
+                    <img className="w-[1423px] h-[70px] left-[0px] top-[0px]" src="Vine.png" />
                 </div>
                 <div className="h-[319px] pb-1.5 left-[161px] top-[386px] absolute flex-col justify-start items-start gap-3.5 inline-flex">
                     <div className="w-[419px] h-[50px] text-[#412207] text-[40px] font-normal font-['Lexend']">How it works:</div>
@@ -38,12 +34,10 @@ const DashboardPage: React.FC = () => {
                 </div>
                 <div className="w-[1238px] h-[466px] left-[11px] top-[867px] absolute">
                     <div className="origin-top-left rotate-[1.93deg] w-[1233px] h-[156.27px] left-[5.25px] top-[259px] absolute">
-                        <img className="w-[450px] h-[122.27px] left-[-1.14px] top-[33.98px] absolute origin-top-left rotate-[1.93deg]" src="https://via.placeholder.com/450x122" />
-                        <img className="w-[450px] h-[122.27px] left-[387.18px] top-[31.02px] absolute origin-top-left rotate-[1.93deg]" src="https://via.placeholder.com/450x122" />
-                        <img className="w-[450px] h-[122.27px] left-[782.56px] top-[26.30px] absolute origin-top-left rotate-[1.93deg]" src="https://via.placeholder.com/450x122" />
+                        <img className="w-[1400px] h-[115px] left-[-1.14px] top-[33.98px] absolute origin-top-left rotate-[-2.06deg]" src="SparkleVine.png" />
                     </div>
                     <div className="w-[811px] h-[444.71px] left-[227px] top-0 absolute">
-                        <img className="w-[300px] h-[430.71px] left-[511px] top-[14px] absolute" src="https://via.placeholder.com/300x431" />
+                        <img className="w-[300px] h-[430.71px] left-[511px] top-[14px] absolute" src="Annette.png" />
                         <div className="w-[553px] h-[151px] left-0 top-0 absolute bg-[#f7ca98] rounded-[40px] border-4 border-[#402207]" />
                         <div className="w-[489px] h-[131px] left-[41px] top-[12px] absolute"><span className="text-[#402207] text-[32px] font-semibold font-['Lexend']">Hello, I’m Annette. <br /></span><span className="text-[#402207] text-[32px] font-light font-['Lexend']">I will be taking notes along side you to guide your research.</span></div>
                         <div className="w-[453px] h-[107px] left-[76px] top-[179px] absolute bg-[#dda15e] rounded-[40px] border-4 border-[#402207]" />
@@ -68,21 +62,15 @@ const DashboardPage: React.FC = () => {
                     </div>
                 </div>
                 <div className="w-[1208.29px] h-[105.91px] left-0 top-[724.83px] absolute">
-                    <img className="w-[450.01px] h-[84.32px] left-[2.91px] top-[5.50px] absolute origin-top-left rotate-[2.06deg]" src="https://via.placeholder.com/450x84" />
-                    <img className="w-[450.01px] h-[84.32px] left-[380.21px] top-[3.75px] absolute origin-top-left rotate-[2.06deg]" src="https://via.placeholder.com/450x84" />
-                    <img className="w-[450.01px] h-[84.32px] left-[758.57px] top-0 absolute origin-top-left rotate-[2.06deg]" src="https://via.placeholder.com/450x84" />
+                    <img className="w-[1423px] h-[70px] left-[2.91px] top-[5.50px]" src="Vine.png" />
                 </div>
                 <div className="w-[1249px] h-[219.72px] left-[-15px] top-[127px] absolute">
                     <div className="w-[1237px] h-[60.72px] left-0 top-[159px] absolute">
-                        <img className="w-[450px] h-[60.72px] left-0 top-0 absolute" src="https://via.placeholder.com/450x61" />
-                        <img className="w-[450px] h-[60.72px] left-[787px] top-0 absolute" src="https://via.placeholder.com/450x61" />
-                        <img className="w-[450px] h-[60.72px] left-[393px] top-0 absolute" src="https://via.placeholder.com/450x61" />
+                        <img className="w-[1400px] h-[50px] left-0 top-0 absolute" src="Sparkle.png" />
                     </div>
                     <div className="left-[228px] top-[47px] absolute text-[#412207] text-[80px] font-bold font-['Lexend']">Prepare for Success</div>
                     <div className="w-[1237px] h-[60.72px] left-[12px] top-0 absolute">
-                        <img className="w-[450px] h-[60.72px] left-0 top-0 absolute" src="https://via.placeholder.com/450x61" />
-                        <img className="w-[450px] h-[60.72px] left-[787px] top-0 absolute" src="https://via.placeholder.com/450x61" />
-                        <img className="w-[450px] h-[60.72px] left-[393px] top-0 absolute" src="https://via.placeholder.com/450x61" />
+                        <img className="w-[1400px] h-[50px] left-0 top-0 absolute" src="Sparkle.png" />
                     </div>
                 </div>
             </div>
