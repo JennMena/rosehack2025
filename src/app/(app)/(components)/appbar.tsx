3e1d81947dafb6f111d65df8f086110560f57f32
Jennifer Mena <mena.mallerly@gmail.com>
2025-01-26 09:58:09 -0800

    Change color navbar

--- src/app/(app)/(components)/appbar.tsx
+++ src/app/(app)/(components)/appbar.tsx
@@ -34,7 +34,7 @@ const AppBar: React.FC = () => {
                 className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
                 aria-label="Sidebar"
             >
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100 dark:bg-gray-800 shadow-lg">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-[#f7ca98] dark:bg-gray-800 shadow-lg">
                     <a href="#" className="flex items-center ps-2.5 mb-5">
                         <img
                             src="/Logo.png"
